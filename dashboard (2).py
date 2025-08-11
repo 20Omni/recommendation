5f7c4be056806c15caef32bbfda5d93371f5aeff
20Omni <2005omni@gmail.com>
2025-08-11 21:58:24 +0530

    Update dashboard (2).py

--- dashboard (2).py
+++ dashboard (2).py
@@ -1,8 +1,4 @@
-import streamlit as st
-import pickle
-import pandas as pd
-import os
-import gdown
+
 
 # ===== Download pickle from Google Drive if missing =====
 file_id = "16xePMUk_UXm_Bc2HAtMFiCfIUD0i2zkD"
